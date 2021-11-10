09821486c2514361e49005d1ac9a8d0b57768131
PawelBis <bis1pawel1@gmail.com>
2021-11-10 08:37:40 +0100

    Add 'ensure_installed' list to treesitter config

--- init.lua
+++ init.lua
@@ -35,6 +35,10 @@ require("packer").startup(function()
 end)
 
 require("nvim-treesitter.configs").setup {
+  ensure_installed = {
+    "rust",
+    "lua",
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,

537d806e7f996a2d9d08e325633e7e86e25ac5a5
Paweł Bis <bis1pawel1@gmail.com>
2022-08-21 16:38:42 +0200

    Change alacritty to kitty

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 ln -s $HOME/.dotfiles/config/nvim $XDG_CONFIG_HOME/
 ln -s $HOME/.dotfiles/config/tmux $XDG_CONFIG_HOME/
-ln -s $HOME/.dotfiles/config/alacritty $XDG_CONFIG_HOME/
+ln -s $HOME/.dotfiles/config/kitty $XDG_CONFIG_HOME/

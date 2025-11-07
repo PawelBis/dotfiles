00c3e9f3d5ac5becab368a4247ed9efa3ae46f83
PawelBis <24803993+PawelBis@users.noreply.github.com>
2025-11-07 10:38:03 +0100

    nvim: highlight on yank

--- dot-config/nvim/lua/config/autocommands.lua
+++ dot-config/nvim/lua/config/autocommands.lua
@@ -1,7 +1,14 @@
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-augroup("__formatter__", { clear = true })
-autocmd({ "BufWritePost" }, {
+vim.api.nvim_create_augroup("__formatter__", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	group = "__formatter__",
 	command = ":FormatWrite",
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	pattern = "*",
+	desc = "highlight selection on yank",
+	callback = function()
+		vim.highlight.on_yank({ timeout = 200, visual = true })
+	end,
 })
